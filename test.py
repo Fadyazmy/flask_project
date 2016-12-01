604ddcf229f8aa76ad4da4dfe31d2a2eab59afaa
fadyazmy <fadel@uwo.ca>
2016-12-01 17:25:47 -0500

    adding bug, doesn't show on db

--- test.py
+++ test.py
@@ -20,23 +20,75 @@ def home():
 
 
 
-@app.route('/movies')
-def movies(name=None):
-    return render_template('form.html', name=name)
+@app.route('/back-end/<type>')
+def movies(type=None):
+    return render_template('form.html', formType=type)
 
-@app.route('/submit', methods=["POST"])
-def submit():
+
+
+
+
+@app.route('/submit/<subType>', methods=["POST"])
+def submit(subType=None):
     cnx = mysql.connector.connect(user='root', database='MovieTheatre')
     cursor = cnx.cursor()
-    insert_stmt = (
-        "INSERT INTO Customer (firstname, lastname, EmailAddress, Sex) "
-        "VALUES (%s, %s, %s, %s)"
-    )
-    data = (request.form['firstname'], request.form['lastname'], request.form['email'], request.form['sex'])
+    insert_stmt = None
+    data = None
+    test = None
+    if subType == "movie":
+        insert_stmt = (
+            "INSERT INTO Movie (MovieName, MovieYear) "
+            "VALUES (%s, %d)"
+        )
+        data = (request.form['movieName'], request.form['movieYear'])
+        test = request.form['movieName']
+    elif subType == "genre":
+        insert_stmt = (
+            "INSERT INTO GENRE (Genre, Movie_idMovie) "
+            "VALUES (%s, %d)"
+        )
+        data = (request.form['Genre'], request.form['Movie_idMovie'])
+        test = request.form['Genre']
+    elif subType == "showing":
+        insert_stmt = (
+            "INSERT INTO Showing (Genre, Movie_idMovie) "
+            "VALUES (%s, %d)"
+        )
+        data = (request.form['movieName'], request.form['Movie_idMovie'])
+    elif subType == "customer":
+        insert_stmt = (
+            "INSERT INTO Customer (Genre, Movie_idMovie) "
+            "VALUES (%s, %d)"
+        )
+        data = (request.form['movieName'], request.form['Movie_idMovie'])
+    elif subType == "attend":
+        insert_stmt = (
+            "INSERT INTO Customer (Genre, Movie_idMovie) "
+            "VALUES (%s, %d)"
+        )
+        data = (request.form['movieName'], request.form['Movie_idMovie'])
+
     cursor.execute(insert_stmt, data)
     cnx.commit()
     cnx.close()
-    return render_template('index.html', firstname=request.form['firstname'], lastname=request.form['lastname'], email=request.form['email'], sex=request.form['sex'])
+    return render_template('index2.html', formType=subType, test = test)
+
+
+
+
+# @app.route('/submit', methods=["POST"])
+# def submit():
+#     cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+#     cursor = cnx.cursor()
+#     insert_stmt = (
+#         "INSERT INTO Customer (firstname, lastname, EmailAddress, Sex) "
+#         "VALUES (%s, %s, %s, %s)"
+#     )
+#     data = (request.form['firstname'], request.form['lastname'], request.form['email'], request.form['sex'])
+#     cursor.execute(insert_stmt, data)
+#     cnx.commit()
+#     cnx.close()
+#     return render_template('index.html', firstname=request.form['firstname'], lastname=request.form['lastname'], email=request.form['email'], sex=request.form['sex'])
 
 @app.route('/sqlInjection')
 def sqlInjection(name=None):
